2f5453910a7a7bad6626fb8f828370b5c33691ff
mygodcatisfat <41024027+mygodcatisfat@users.noreply.github.com>
2025-06-26 16:39:35 +0800

    Update darkmode.js
    註解

--- darkmode.js
+++ darkmode.js
@@ -1,3 +1,8 @@
+/*
+控制網頁的夜間模式（切換 body class）
+記憶使用者選擇（localStorage）
+頁面載入時自動套用上次選擇
+*/
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
     localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));

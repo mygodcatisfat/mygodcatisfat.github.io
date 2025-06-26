3ea7e724715f81a6b725828bd9f7d32a6e16bc91
mygodcatisfat <41024027+mygodcatisfat@users.noreply.github.com>
2025-06-26 16:57:53 +0800

    Update gallery.js
    註解

--- gallery.js
+++ gallery.js
@@ -1,3 +1,6 @@
+/*
+複製相簿內圖片，讓輪播動畫可以無縫循環
+*/
 document.addEventListener('DOMContentLoaded', () => {
     const marquee = document.querySelector('.gallery-marquee');
     if (marquee) marquee.innerHTML += marquee.innerHTML;
